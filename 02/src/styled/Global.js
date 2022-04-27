08d280c775e6a8384e95c327f7ffda2d8c52be6c
Malgorzata Kowacka <kowackag@gmail.com>
2022-04-27 19:35:04 +0200

    Add fonts

--- 02/src/styled/Global.js
+++ 02/src/styled/Global.js
@@ -1,4 +1,10 @@
 import {createGlobalStyle} from 'styled-components';
+import latoRegularWoff from "./../fonts/lato-regular-webfont.woff";
+import latoRegularWoff2 from "./../fonts/lato-regular-webfont.woff2";
+import latoBoldWoff from "./../fonts/lato-bold-webfont.woff";
+import latoBoldWoff2 from "./../fonts/lato-bold-webfont.woff2";
+import latoItalicWoff2 from "./../fonts/lato-italic-webfont.woff2";
+import latoItalicWoff from "./../fonts/lato-italic-webfont.woff";
 
 const GlobalStyle = createGlobalStyle`
     :root {
@@ -7,6 +13,30 @@ const GlobalStyle = createGlobalStyle`
         --color-beta: ${props=>props.theme.colorLight};
         --color-contrast:${props=>props.theme.colorContrast};
         --color-line:${props=>props.theme.colorLine};
+    }
+
+    @font-face {
+        font-family: "Roboto";
+        font-style: regular;
+        font-weight: 400;
+        src:
+        url(${latoRegularWoff2}) format('woff2'),
+        url(${latoRegularWoff}) format('woff');
+    }
+    @font-face {
+        font-family: "Roboto";
+        font-weight: 700;
+        src:
+        url(${latoBoldWoff2}) format('woff2'),
+        url(${latoBoldWoff}) format('woff');
+    }
+    @font-face {
+        font-family: "Roboto";
+        font-style: italic;
+        font-weight: 400;
+        src:
+        url(${latoItalicWoff2}) format('woff2'),
+        url(${latoItalicWoff}) format('woff');
     }
 
     html {
