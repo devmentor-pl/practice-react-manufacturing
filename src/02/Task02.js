99ba3f3987c75f7b03d50df42e60259c7ca61104
Andrzej <andis_as@wp.pl>
2022-11-08 20:07:05 +0100

    fix Task02

--- src/02/Task02.js
+++ src/02/Task02.js
@@ -8,7 +8,11 @@ const Task02 = () => {
         <div>zad2</div>
         <div>branch operations - zad1 and zad2 merged to develop and develop to master</div>
         <div>
-          <a href="https://github.com/andrzej-stasinski/practice-react-manufacturing/blob/master/Changelog.md" target="_blank" >Changelog</a>
+          <a 
+            href="https://github.com/andrzej-stasinski/practice-react-manufacturing/blob/master/Changelog.md" 
+            target="_blank" 
+            rel="noreferrer"
+          >Changelog</a>
         </div>
     </div>
   )
