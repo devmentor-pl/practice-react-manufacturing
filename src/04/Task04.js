09373b8548c1acfb1230fa96fd7134565f0bfba2
Andrzej <andis_as@wp.pl>
2022-11-09 17:44:43 +0100

    added links to site of hosting Netlify & dev server

--- src/04/Task04.js
+++ src/04/Task04.js
@@ -3,16 +3,27 @@ import React from 'react'
 const Task04 = () => {
   return (
     <div>
-        <h2>Task 04</h2>
+        <h2>Task 04 Netlify hosting and Function for hiding key API</h2>
         <div>hosting by Netlify</div>
         <div>
-        </div>
             <a 
                 href="https://golden-alpaca-49beb5.netlify.app/"
                 target="_blank" 
                 rel="noreferrer" 
             >site in Netlify
             </a>
+        </div>
+        <br />
+        <div>
+            <div>run app in terminal</div>
+            <div>netlify dev</div>
+            <a 
+                href="http://localhost:8888/"
+                target="_blank" 
+                rel="noreferrer" 
+            >run as dev
+            </a>
+        </div>
     </div>
   )
 }
