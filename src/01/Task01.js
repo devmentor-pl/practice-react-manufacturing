abe3f7fd46cdfbc1e9db0917c0730b9c7bebdd70
Joanna Wierzbicka <joannawierzbicka@poczta.onet.pl>
2022-11-13 18:21:27 +0100

    task 1 done

---
+++ src/01/Task01.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Trello from './Trello'
+
+export const Task01 = () => {
+    return (
+        <div>
+            <h2>Task1</h2>
+            <Trello/>
+        </div>
+    )
+}
+
+export default Task01
