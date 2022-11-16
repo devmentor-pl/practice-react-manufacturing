b89eae14dfc4ae59573d245ff8e4f6ddf2781a47
Joanna Wierzbicka <joannawierzbicka@poczta.onet.pl>
2022-11-16 11:04:01 +0100

    all tasks done

--- src/01/Trello.js
+++ src/01/Trello.js
@@ -1,7 +1,7 @@
 export const Trello = () => {
     return (
         <div>
-            <a href={'https://trello.com/invite/b/wrUwpAC0/ATTIf0e2a5b56a81c1398ddf2491de8acf88A533BD5B/project-1'} target={'blank'}>MY TRELLO BORAD</a>
+            <a href={'https://trello.com/invite/b/wrUwpAC0/ATTIf0e2a5b56a81c1398ddf2491de8acf88A533BD5B/project-1'} target={'blank'}>MY TRELLO BOARD</a>
         </div>
     )
 }
