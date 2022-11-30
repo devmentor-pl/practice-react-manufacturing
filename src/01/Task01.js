62452353dfa12af29c835f76d4be88fb0d3df8c2
Camila Montes Celinski <camicelinski@gmail.com>
2022-11-30 22:36:23 +0100

    Finish task 02

--- src/01/Task01.js
+++ src/01/Task01.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Task01 = () => {
   return (
     <section>
-      <h2>Task 01 - Trello</h2>
+      <h1>Task 01 - Trello</h1>
       <p>
         <a href="https://trello.com/b/i1Jo7adf/my-meal-planner">Trello table - final project: my meal planner</a>
       </p>
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Task01 from './01/Task01'
+import Task02 from './02/Task02'
 
 const App = () => {
   return (
     <main>
       <Task01 />
+      <Task02 />
     </main>
   )
 }
