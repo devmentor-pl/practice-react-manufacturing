28ff49b99cf58e5cc18a79ae35ce0cda5408c4b9
Camila Montes Celinski <camicelinski@gmail.com>
2022-12-05 19:08:09 +0100

    Set GitHub actions

---
+++ src/03/Task03.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Task03 = () => {
+  return (
+    <section>
+      <h1>Task 03 - gh-pages</h1>
+      <p>
+        <a href="https://camicelinski.github.io/practice-react-manufacturing">GitHub Pages website with GitHub Actions</a>
+      </p>
+    </section>
+  )
+}
+
+export default Task03
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Task01 from './01/Task01'
 import Task02 from './02/Task02'
+import Task03 from './03/Task03'
 
 const App = () => {
   return (
     <main>
       <Task01 />
       <Task02 />
+      <Task03 />
     </main>
   )
 }
