a6982fac1646abccf83b2742b954dbb8acef371d
Camila Montes Celinski <camicelinski@gmail.com>
2022-12-06 20:23:23 +0100

    Add file netlify.toml

---
+++ src/04/Task04.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Task04 = () => {
+  return (
+    <section>
+      <h1>Task 04 - Netlify</h1>
+      <p>
+        <a href="https://practice-react-manufacturing-camiceli.netlify.app/">Website</a> hosted on Netlify
+      </p>
+    </section>
+  )
+}
+
+export default Task04
