3a23a94789774cf9ab35537db2cbbfa0c243f742
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-11-05 13:13:57 +0100

    Task 2.2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export const App = () => {
         <a href="https://trello.com/b/7Sqi8ozK/task1" target="blank">
           TRELLO BOARD
         </a>
-        <h1>Task 2.1</h1>
+        <h1>Task 2.2</h1>
       </div>
     </div>
   );
