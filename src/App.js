a3929d45cfeb2b4388509674cf6a1a1f9d2f11a5
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-11-05 13:04:05 +0100

    task 1 added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div >
+      <div>
+        <h1>
+          Task 1
+        </h1>
+        <a href="https://trello.com/b/7Sqi8ozK/task1" target="blank">
+          TRELLO BOARD
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
