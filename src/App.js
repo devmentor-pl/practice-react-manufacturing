b91199c973ab13433a5c51e8642adc6286ee8d53
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-11-05 16:41:00 +0100

    task 4 added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import React from "react";
 export const App = () => {
   return (
     <div >
-      <div>
-        <h1>
-          Task 1
-        </h1>
-        <a href="https://trello.com/b/7Sqi8ozK/task1" target="blank">
-          TRELLO BOARD
-        </a>
-        <h1>Task 2.2</h1>
-        <h1>Task 3.19</h1>
-      </div>
+      <h1>
+        Task 1
+      </h1>
+      <a href="https://trello.com/b/7Sqi8ozK/task1" target="blank">
+        TRELLO BOARD
+      </a>
+      <h1>Task 2.2</h1>
+      <h1>Task 3.19</h1>
+      <h1>Task 4</h1>
+      <p></p>
     </div>
   );
 }
