2202c2b438692f9114382c2cd4c795b26b7a6d7d
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-11-05 16:50:32 +0100

    4.2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ export const App = () => {
       </a>
       <h1>Task 2.2</h1>
       <h1>Task 3.19</h1>
+      <a href="https://sebastian-wapinski.github.io/practice-react-manufacturing/">github</a>
       <h1>Task 4.1</h1>
+      <a href="https://master--cheerful-kelpie-eab04a.netlify.app/">netlify</a>
     </div>
   );
 }
