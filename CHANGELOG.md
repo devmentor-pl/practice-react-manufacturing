c423c3ceb4a18bfd1125fedf96416271392d864e
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-11-05 14:18:01 +0100

    key added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - ghPages and Actions added
+- key added
 
 ## [0.2.0] - 2023-11-05
 
--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export const App = () => {
           TRELLO BOARD
         </a>
         <h1>Task 2.2</h1>
-        <h1>Task 3</h1>
+        <h1>Task 3.1</h1>
       </div>
     </div>
   );
