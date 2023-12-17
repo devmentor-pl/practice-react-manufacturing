24f686709e81a4d609fcb8ef57c685c950d997e8
Lukasz <l.bdnski@gmail.com>
2023-12-17 19:07:21 +0100

    add tasks

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div>My Trello: <a href={'https://trello.com/b/D1Uh10Wp/bookingapp'} target={'blank'}>My Trello</a></div>
+    <div>App: <a href={'https://amazing-mousse-d3b288.netlify.app/'} target={'blank'}>My Trello</a></div>
+    </>
+  );
+}
+
+export default App;
