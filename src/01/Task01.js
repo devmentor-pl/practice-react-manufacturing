3498357c4544b3e0d550420dfaf2c3240c9f9b9c
Maciej Necka <macieknecka@gmail.com>
2024-02-28 10:24:06 +0100

    Task 01 - Done

---
+++ src/01/Task01.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Task01 = () => {
+  return (
+    <div>
+      <h2>Task01</h2>
+      <a
+        href="https://trello.com/b/15c6RXUe/react-manufacturing"
+        target="blank"
+      >
+        practice-react-manufacturing
+      </a>
+    </div>
+  );
+};
+
+export default Task01;
