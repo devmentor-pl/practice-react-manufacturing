2b7c1a3df419e9f44d2411b71037ed47ecad9e68
Maciej Necka <macieknecka@gmail.com>
2024-02-28 12:50:16 +0100

    Task 04 - Netlify

---
+++ src/04/Task04.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Task04 = () => {
+  return (
+    <div>
+      <h2>Task04</h2>
+      <a
+        href="https://maciejnecka.github.io/practice-react-manufacturing/"
+        target="blank"
+      >
+        Netlify
+      </a>
+    </div>
+  );
+};
+
+export default Task04;
