9bf4c7e77920e189f17fbca563add3ee16f57f31
Paweł Suder <pawel.suder94@gmail.com>
2024-11-11 15:29:33 +0100

    task 01 done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div>
+      <div>
+        <h1>Task 1</h1>
+        <a href="https://trello.com/b/sKS9jb8a/soloproject" target="blank">
+          TRELLO BOARD
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default App;
